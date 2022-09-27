d4138ae5708e5a4d6d4bb1b501793cd0aa64892c
Luke Pighetti <lukepighetti@gmail.com>
2022-09-27 18:02:33 -0400

    backend: push latest rankings

--- backend/content.js
+++ backend/content.js
@@ -1,138 +1,138 @@
 let options = [
   {
-    id: 7,
-    rank: 1379,
-    name: "riverpod",
-  },
-  {
-    id: 2,
-    rank: 1350,
-    name: "provider",
-  },
-  {
-    id: 27,
-    rank: 1249,
-    name: "get_it_mixin",
-  },
-  {
-    id: 4,
-    rank: 1219,
-    name: "get_it",
-  },
-  {
-    id: 20,
-    rank: 1198,
-    name: "state_notifier",
-  },
-  {
-    id: 26,
-    rank: 1147,
-    name: "momentum",
-  },
-  {
-    id: 19,
-    rank: 1057,
-    name: "flutter_clean_architecture",
-  },
-  {
-    id: 29,
-    rank: 1044,
-    name: "binder",
-  },
-  {
-    id: 22,
-    rank: 1034,
-    name: "mvc_pattern",
-  },
-  {
-    id: 30,
-    rank: 1028,
-    name: "flutter_command",
-  },
-  {
-    id: 17,
-    rank: 1026,
-    name: "states_rebuilder",
-  },
-  {
-    id: 25,
-    rank: 1016,
-    name: "elementary",
-  },
-  {
-    id: 6,
-    rank: 1006,
-    name: "rxdart",
-  },
-  {
-    id: 8,
-    rank: 998,
-    name: "stacked",
-  },
-  {
-    id: 14,
-    rank: 993,
-    name: "flutter_mobx",
-  },
-  {
-    id: 12,
-    rank: 974,
-    name: "mobx",
-  },
-  {
-    id: 11,
-    rank: 972,
-    name: "flutter_modular",
-  },
-  {
     id: 5,
-    rank: 964,
+    rank: 1340,
     name: "bloc",
   },
   {
-    id: 21,
-    rank: 936,
-    name: "scoped_model",
-  },
-  {
-    id: 18,
-    rank: 894,
-    name: "redux",
-  },
-  {
-    id: 23,
-    rank: 845,
-    name: "async_redux",
-  },
-  {
-    id: 13,
-    rank: 825,
-    name: "injectable",
-  },
-  {
     id: 1,
-    rank: 810,
+    rank: 1339,
     name: "getx",
   },
   {
-    id: 10,
-    rank: 789,
-    name: "velocity_x",
+    id: 2,
+    rank: 1325,
+    name: "provider",
   },
   {
-    id: 24,
-    rank: 771,
-    name: "kiwi",
-  },
-  {
-    id: 16,
-    rank: 750,
-    name: "flutter_redux",
+    id: 7,
+    rank: 1321,
+    name: "riverpod",
   },
   {
     id: 28,
-    rank: 726,
+    rank: 1301,
     name: "fish_redux",
+  },
+  {
+    id: 4,
+    rank: 1300,
+    name: "get_it",
+  },
+  {
+    id: 22,
+    rank: 1286,
+    name: "mvc_pattern",
+  },
+  {
+    id: 20,
+    rank: 1150,
+    name: "state_notifier",
+  },
+  {
+    id: 29,
+    rank: 1133,
+    name: "binder",
+  },
+  {
+    id: 6,
+    rank: 1127,
+    name: "rxdart",
+  },
+  {
+    id: 13,
+    rank: 1037,
+    name: "injectable",
+  },
+  {
+    id: 12,
+    rank: 1008,
+    name: "mobx",
+  },
+  {
+    id: 17,
+    rank: 970,
+    name: "states_rebuilder",
+  },
+  {
+    id: 8,
+    rank: 965,
+    name: "stacked",
+  },
+  {
+    id: 18,
+    rank: 953,
+    name: "redux",
+  },
+  {
+    id: 24,
+    rank: 938,
+    name: "kiwi",
+  },
+  {
+    id: 30,
+    rank: 914,
+    name: "flutter_command",
+  },
+  {
+    id: 26,
+    rank: 887,
+    name: "momentum",
+  },
+  {
+    id: 21,
+    rank: 864,
+    name: "scoped_model",
+  },
+  {
+    id: 25,
+    rank: 861,
+    name: "elementary",
+  },
+  {
+    id: 23,
+    rank: 805,
+    name: "async_redux",
+  },
+  {
+    id: 11,
+    rank: 775,
+    name: "flutter_modular",
+  },
+  {
+    id: 16,
+    rank: 774,
+    name: "flutter_redux",
+  },
+  {
+    id: 19,
+    rank: 764,
+    name: "flutter_clean_architecture",
+  },
+  {
+    id: 14,
+    rank: 694,
+    name: "flutter_mobx",
+  },
+  {
+    id: 27,
+    rank: 632,
+    name: "get_it_mixin",
+  },
+  {
+    id: 10,
+    rank: 537,
+    name: "velocity_x",
   },
 ];
 
